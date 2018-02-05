a4a4be1d3b0d322ebd8ed9cfe010f836cd84f7d3
Nathan Ju <34ndju@gmail.com>
2018-02-04 23:10:46 -0600

    stemming and counting

--- grab_lyrics.py
+++ grab_lyrics.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup as bs
 import urllib2
 
+from collections import Counter
 import metapy
 from authentication import Secrets
 from pymongo import MongoClient
@@ -9,7 +10,7 @@ secrets = Secrets()
 
 
 link = "https://www.azlyrics.com/lyrics/fountainsofwayne/stacysmom.html"
-link = "https://www.azlyrics.com/lyrics/panicatthedisco/thisisgospel.html"
+link = "https://www.azlyrics.com/lyrics/logic/18002738255.html"
 
 def get_lyrics(link):
     out = ""
@@ -18,25 +19,42 @@ def get_lyrics(link):
     for x in soup.find_all("br"):
         try:
             string = str(x.next_sibling.encode('utf-8'))
-            if len(string.strip()) > 1 and string.find("<br/>") == -1:
+            if len(string.strip()) > 1 and string.find(">") == -1:
                 out += string.strip() + " "
         except:
             continue
 
     return out
 
-def tokenize(lyrics):
+def parse_and_count(link):
+
+    tag_suppression = True
+    n = 1
+    min_token_len = 4
+    max_token_len = 30
+
+    lyrics = get_lyrics(link)
+
     doc = metapy.index.Document()
     doc.content(lyrics)
 
-    tok = metapy.analyzers.ICUTokenizer()
-    tok.set_content(lyrics)
-    tokens = [token for token in tok]
-    print tokens
+    tok = metapy.analyzers.ICUTokenizer(suppress_tags= tag_suppression)
+    tok = metapy.analyzers.LengthFilter(tok, min=min_token_len, max=max_token_len)
+    tok = metapy.analyzers.ListFilter(tok, "lemur-stopwords.txt", metapy.analyzers.ListFilter.Type.Reject)
+    tok = metapy.analyzers.Porter2Filter(tok)
+    tok = metapy.analyzers.LowercaseFilter(tok)
 
-lyrics = get_lyrics(link)
-print lyrics
-tokenize(lyrics)
+    tok.set_content(lyrics)
+    #tokens = [token for token in tok]
+
+    ana = metapy.analyzers.NGramWordAnalyzer(n, tok)
+    ngrams = ana.analyze(doc)
+    print ngrams
+
+    return ngrams
+
+
+parse_and_count(link)
 
 
 
